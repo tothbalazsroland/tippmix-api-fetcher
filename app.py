3b34c775de696b03ad543743194024bcb2e3a648
Toth <toth.balazs.roland@gmail.com>
2019-03-18 16:09:45 +0100

    / route gives back the json from the tippmix api

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import urllib2
+import json
+from flask import jsonify
 
 app = Flask(__name__)
-
+tippmix_api_url = "http://api.tippmix.hu/tippmix/search"
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    response = urllib2.urlopen(tippmix_api_url).read()
+    content = json.loads(response)
+    return jsonify(content)
 
 
 if __name__ == '__main__':
